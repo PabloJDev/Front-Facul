802b4f80010d9c4de0d5e87c610a88caa0030af8
Pablo Jorge <pablodosantos2007@gmail.com>
2025-10-16 21:21:28 -0300

    Add files via upload

--- HTML-Atividade 05/script.js
+++ HTML-Atividade 05/script.js
@@ -14,4 +14,20 @@ function verificarRefeicao() {
     }
 
     paragrafoMensagem.innerHTML = resultado;
+}
+
+function botaoFor() {
+    let paragrafoText = document.getElementById('text');
+
+    for (let i = 0; i < 5; i++) {
+        paragrafoText.innerHTML += i;
+        console.log(i);
+    }
+
+    let lista = ["arroz", "salada", "picanha" ]
+    for (let index = 0; index < lista.length; index++) {
+        const element = lista[index];
+        paragrafoText.innerHTML += `<br>${element}`;
+    }
+
  }
